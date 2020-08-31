1d307c109348130f9b91c76f4c40bef007ff63bb
Lhwhatever <llinhong.2301+github@gmail.com>
2020-08-31 23:08:09 +0800

    Change redirectUri for preview/production

--- utils/serverside.ts
+++ utils/serverside.ts
@@ -1,2 +1,8 @@
+const hosts = {
+    development: 'http://to.localhost:3000',
+    preview: 'https://splyse.lhwhatever.vercel.app',
+    production: `https://${process.env.VERCEL_URL}`,
+} as Record<string, string>;
+
 export const stateKey = 'splyse-state';
-export const redirectUri = 'http://to.localhost:3000/api/callback';
+export const redirectUri = hosts[process.env.DEPLOYMENT_ENV || 'development'] + '/api/callback';
