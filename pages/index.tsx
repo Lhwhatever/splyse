ed972afe29e1ae11b15ef3db2e8ce09e272ffe46
Lhwhatever <llinhong.2301+github@gmail.com>
2020-08-31 22:46:53 +0800

    Allow state connection in / to be undefined
    This is to represent the initial state when the browser has not
    established whether it is connected to Spotify (e.g. because the call to
    Spotify's API server has not responded yet).
    
    The intention is for such a state to be representable using e.g.
    spinner / progress bar so that users will not be confused.

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,18 +10,19 @@ import SpotifyConnection, { UserProfile } from 'utils/spotify';
 
 export default function Home(): JSX.Element {
     const [alert, setAlert] = React.useState<AlertState | null>(null);
-    const [connection, setConnection] = React.useState<SpotifyConnection | null>(null);
+    const [connection, setConnection] = React.useState<SpotifyConnection | null | undefined>(undefined);
     const [userProfile, setUserProfile] = React.useState<UserProfile | null>(null);
     const router = useRouter();
 
     React.useEffect(() => {
-        if ('error' in router.query) {
-            handleAuthError(router.query.error, setAlert);
-            setConnection(null);
-        } else if (typeof router.query.access_token === 'string' && typeof router.query.refresh_token === 'string') {
-            console.log(router.query);
+        if (typeof router.query.access_token === 'string' && typeof router.query.refresh_token === 'string') {
             const connection = new SpotifyConnection(router.query.access_token, router.query.refresh_token);
             handleAuthSuccess(connection, setAlert, setConnection, setUserProfile);
+        } else if ('error' in router.query) {
+            handleAuthError(router.query.error, setAlert);
+            setConnection(null);
+        } else {
+            setConnection(null);
         }
     }, [router]);
 
