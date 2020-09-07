09085ec004b87e0288134f4e876fadd841b77d92
Lhwhatever <llinhong.2301+github@gmail.com>
2020-09-07 22:54:39 +0800

    AppThunk will now accept type arguments
    This is to allow dispatch(thunk(...)) to return values (for instance, Promises)
    for better control flow options (especially for async operations).

--- store/store.ts
+++ store/store.ts
@@ -1,9 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import rootReducer from './root';
 
 export type RootState = ReturnType<typeof rootReducer>;
 const store = configureStore({
     reducer: rootReducer,
 });
+
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<T = void> = ThunkAction<T, RootState, unknown, Action<string>>;
 
 export default store;
